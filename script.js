bca2e9c4037a58e1e1ba02a9b96e0ba090786711
hellopeople4304 <hellopeople4304@gmail.com>
2025-02-19 14:16:08 -0500

    script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById('bike-application-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for your application! We will get back to you soon.');
+    // Here you can add code to send the form data to a server or email
+});
+
+document.getElementById('donate-button').addEventListener('click', function() {
+    alert('Thank you for your willingness to donate! Please visit our donation page.');
+    // Here you can redirect to a donation page or open a payment gateway
+});
